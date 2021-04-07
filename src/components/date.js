c3fb80aec192ef7ee23a7c5699e12e8b3d0f737f
amliu <amliu@umich.edu>
2021-04-07 17:35:23 -0500

    date and time widget

--- src/components/date.js
+++ src/components/date.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { useState , useEffect } from 'react'
 
 import '../css/App.css';
+import '../css/date.css';
 
-export default class Date extends React.Component {
-  render() {
-    return (
-      <p>today is monday</p>
-    )
-  }
+export default function TimeDate() {
+  var [date, setDate] = useState(new Date());
+
+  const options = { weekday: 'long', month: 'long', day: 'numeric' };
+  
+  useEffect(() => {
+      var timer = setInterval(() => setDate(new Date()), 1000)
+      return function() {
+          clearInterval(timer)
+      }
+  });
+
+  return (
+    <div>
+      <h1 id='date'>{ date.toLocaleDateString(undefined, options) }</h1>
+      <h2 id='time'>{ date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) }</h2>
+    </div>
+  )
 }
--- src/components/main.js
+++ src/components/main.js
@@ -3,14 +3,14 @@ import Grid from '@material-ui/core/Grid';
 
 import '../css/App.css';
 import ToDoList from "./todolist";
-import Date from "./date";
+import TimeDate from "./date";
 
 export default function Main() {
   return (
     <main>
       <Grid container='true' container spacing={2}>
         <Grid item xs={12} md={4}>
-          <div className="widget"><Date/></div>
+          <div className="widget"><TimeDate/></div>
           <div className="widget"><ToDoList/></div>
         </Grid>
         <Grid item xs={12} md={8}>
