a76f152f4d3736ca6c9e7b533bd14435ca8e8212
Kristianne Zeer <kzeer@umich.edu>
2021-04-09 15:25:08 -0400

    Debugging todo list add and remove

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -6,22 +6,30 @@ export default class ToDoList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            todoList: ["default item"]
-        }
+            newitem: "",
+            todolist: ["item1", "item2"]
+        };
     }
-    addItem() {
+    addItem(newitem) {
         console.log("Item added")
-        
+        this.state.todolist.append(newitem)
     } // addItem()
+    
+    removeItem() {
+        console.log("Item removed")
+        this.state.todolist.remove()
+    } // removeItem()
     
     render() {
         return (
             <div className = "todo-list">
                 <p>Todo</p>
-                <button onClick = {this.addItem.bind(this)}>Add item</button>
-                <li for todo in todoList>
-                    1
-                </li>
+                <input type = "text" name = "name"/>
+                <input type = "submit" value = "add" onClick={this.addItem.bind(this)}/>
+                <input type = "submit" value = "remove" onClick={this.removeItem.bind(this)}/>
+                <ol>
+                    <li>{this.state.todolist}</li>
+                </ol>
             </div>
         ) // return
     } // render()
