ee9c423b8391522e88e88ccf7e5de1cee1950a72
kevcch <kevincc@umich.edu>
2021-04-14 22:55:21 -0400

    Weather and QOTD fetch fixes

--- src/components/qotd.js
+++ src/components/qotd.js
@@ -5,25 +5,28 @@ import '../css/App.css';
 import '../css/qotd.css';
 
 export default function QOTD() {
-  var [quote, setQuote] = useState({});
+  var [quote, setQuote] = useState({'fetched': false, 'data': {}});
 
   useEffect(() => {
-    fetch("https://quotes.rest/qod", { 'method': 'GET'})
-    .then((response) => {
-      if(!response.ok) throw Error(response.statusText);
-         return response.json();
-    })
-    .then((data) => {
-          setQuote(data.contents.quotes[0])
-          //data.contents.quotes[0].quote + " - " + data.contents.quotes[0].author
-    })
-    .catch((error) => console.log(error));
+    if(!quote.fetched){
+      console.log("Quote Fetching")
+      fetch("https://quotes.rest/qod", { 'method': 'GET'})
+      .then((response) => {
+        if(!response.ok) throw Error(response.statusText);
+           return response.json();
+      })
+      .then((data) => {
+            setQuote({'fetched': true, 'data': data.contents.quotes[0]})
+            //data.contents.quotes[0].quote + " - " + data.contents.quotes[0].author
+      })
+      .catch((error) => console.log(error));
+    }
   });
 
   return (
     <div >
       <h2 id='qotd'>Quote of the Day</h2>
-      <h2 id='quote'>{quote.quote + " - " + quote.author}</h2>
+      <h2 id='quote'>{quote.data.quote + " - " + quote.data.author}</h2>
     </div>
   )
 }
