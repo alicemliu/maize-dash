dd7c2486e4ea2b5ad1e455ff6b80d095e06b81eb
josephar <josephar@umich.edu>
2021-04-16 00:52:18 -0400

    working add functionality

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -6,31 +6,55 @@ export default class ToDoList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            newitem: "",
-            todolist: ["item1", "item2"]
+            item: "",
+            todolist: []
         };
     }
-    addItem(newitem) {
-        console.log("Item added")
-//        this.state.todolist.append(newitem)
+    addItem = (event) => {
+        event.preventDefault();
+
+        //this.state.todolist.append(newitem)
+        this.setState({todolist: this.state.todolist.concat(this.state.item)})
+        /*
+        var newArr = this.state.todolist;
+        newArr.push(this.state.item);
+        this.setState({todolist: newArr});
+        */
     } // addItem()
     
     removeItem() {
         console.log("Item removed")
-//        this.state.todolist.remove()
+        this.state.todolist.remove()
     } // removeItem()
+
+
+    setItem = (event) => {
+        this.setState({item: event.target.value}); 
+    }
     
     render() {
         return (
             <div className = "todo-list">
                 <h2>To Do List</h2>
-                <input type = "text" name = "name"/>
-                <input type = "submit" value = "add" onClick={this.addItem.bind(this)}/>
-                <input type = "submit" value = "remove" onClick={this.removeItem.bind(this)}/>
+                <form onSubmit={this.addItem}>
+                    <input type="text" value={this.state.item} onChange={this.setItem}/>
+                    <button type="submit">Add</button>
+                </form>
+
                 <ol>
-                    <li><p>{this.state.todolist}</p></li>
+                    {this.state.todolist.map((item) => (
+                        <li>{item}</li>
+                    ))}
                 </ol>
             </div>
         ) // return
     } // render()
 }
+
+/*
+<input type = "text" name = "name"/>
+                <input type = "submit" value = "add" onClick={this.addItem.bind(this)}/>
+                <input type = "submit" value = "remove" onClick={this.removeItem.bind(this)}/>
+
+                <li><p>{this.state.todolist}</p></li>
+*/
